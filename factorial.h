f2bb845f2f7fd29b31bb1ade18e83d32acfa397d
prernapahwa <60768072+prernapahwa@users.noreply.github.com>
2022-02-07 12:18:59 +0530

    done

---
+++ factorial.h
@@ -0,0 +1,11 @@
+/contains declarations of the functions used in factorial.c
+//
+
+struct Node;
+
+void large_factorial(struct Node*, int);
+
+void print_in_reverse(struct Node*);
+
+void fact(int);
+© 2022 GitHub, Inc.
